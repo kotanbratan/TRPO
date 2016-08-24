a21a05374962d99372481ceb950562ae8563e7bb
kotanbratan <katerina.ermakova86@gmail.com>
2016-08-24 16:59:13 +0300

    '3'

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
 from myapp import views
-#from myapp.views import MainView, AuthView, BidsList,
+from myapp.views import MainView#, AuthView, BidsList
 
 urlpatterns = [
     url(r'^$', views.MainView, name='main'),
-    url(r'^auth/$', views.AuthView, name='auth'),
-    url(r'^bids/$', views.BidsListView, name='bids_list'),
+    #url(r'^auth/$', views.AuthView, name='auth'),
+    #url(r'^bids/$', views.BidsListView, name='bids_list'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.contrib.auth.models import User
 from django.http import JsonResponse
-from django.contrib.auth import login, logout
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+#from django.contrib.auth import login, logout
+#from django.contrib.auth.decorators import login_required
 
-#from core.patterns.print import ProxyXLSPrinter
 
 # Create your views here.
 def MainView (request):
