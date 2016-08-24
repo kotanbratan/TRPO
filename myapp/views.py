d6520d3e508276792d243a3cd17fe6faa1014890
kotanbratan <katerina.ermakova86@gmail.com>
2016-08-24 16:43:36 +0300

    '3'

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+#from core.patterns.print import ProxyXLSPrinter
 
 # Create your views here.
-def bids_list(request):
-    return render(request, 'myapp/bids_list.html', {})
+def MainView (request):
+    return render(request, 'myapp/MainView.html', {})
