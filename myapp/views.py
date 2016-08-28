4b341b86041a977e194f3322b30f57561a5a58b1
kotanbratan <katerina.ermakova86@gmail.com>
2016-08-28 22:37:28 +0300

    3

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 from django.http import JsonResponse
 #from django.contrib.auth import login, logout
 #from django.contrib.auth.decorators import login_required
@@ -8,4 +8,4 @@ from django.http import JsonResponse
 
 # Create your views here.
 def MainView (request):
-    return render(request, 'myapp/MainView.html', {})
+    return render(request, 'myapp/main.html', {})
