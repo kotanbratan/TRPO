5d8f799b423c9d2ff2ba5e00e608ff8721d44342
kotanbratan <katerina.ermakova86@gmail.com>
2016-08-29 15:22:18 +0300

    4

--- myapp/views.py
+++ myapp/views.py
@@ -4,8 +4,10 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.http import JsonResponse
 #from django.contrib.auth import login, logout
 #from django.contrib.auth.decorators import login_required
+from .models import Bid
 
 
 # Create your views here.
 def MainView (request):
+    #bids = Bid.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'myapp/main.html', {})
